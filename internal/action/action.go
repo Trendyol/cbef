27454f8993f23b136aaeabbb64302c3b556b1438
Serhat Karabulut <serhat.karabulut@trendyol.com>
2023-11-12 21:02:01 +0300

    style: tidy up main

---
+++ internal/action/action.go
@@ -0,0 +1,27 @@
+package action
+
+import (
+	"context"
+	"github.com/couchbase/gocb/v2"
+	"github.com/trendyol/cbef/internal/model"
+)
+
+type Action interface {
+	Upsert(ctx context.Context, f *model.Function) error
+	Status(ctx context.Context) (*gocb.EventingStatus, error)
+	Pause(ctx context.Context, name string) error
+	Deploy(ctx context.Context, name string) error
+	Undeploy(ctx context.Context, name string) error
+	StopFunctions(ctx context.Context, prefix, excludedFunction string) (map[string]struct{}, error)
+	WaitFunctionsProcesses(ctx context.Context, functions map[string]struct{}) error
+}
+
+type action struct {
+	cluster *gocb.Cluster
+}
+
+func NewAction(cluster *gocb.Cluster) Action {
+	return &action{
+		cluster: cluster,
+	}
+}
---
+++ internal/action/wait.go
@@ -0,0 +1,31 @@
+package action
+
+import (
+	"context"
+	"fmt"
+	"github.com/couchbase/gocb/v2"
+	"time"
+)
+
+// WaitFunctionsProcesses waits processes for provided eventing functions.
+func (a *action) WaitFunctionsProcesses(ctx context.Context, functions map[string]struct{}) error {
+	t := time.Tick(500 * time.Millisecond)
+	for range t {
+		if len(functions) == 0 {
+			break
+		}
+
+		statuses, err := a.Status(ctx)
+		if err != nil {
+			return fmt.Errorf("failed to fetch functions statuses: %w", err)
+		}
+
+		for _, fn := range statuses.Functions {
+			if _, ok := functions[fn.Name]; ok && (fn.Status == gocb.EventingFunctionStatePaused || fn.Status == gocb.EventingFunctionStateUndeployed) {
+				delete(functions, fn.Name)
+			}
+		}
+	}
+
+	return nil
+}
