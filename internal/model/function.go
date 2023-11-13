66a2f6f5a82fe861440e36c4a793179ecafd6159
Emre Önal <emmreonal@gmail.com>
2023-11-13 22:29:07 +0300

    refactor: change function model

--- internal/model/function.go
+++ internal/model/function.go
@@ -2,24 +2,20 @@ package model
 
 import "time"
 
-// Function represents eventing function structure.
+// Function represents an eventing function.
 type Function struct {
-	Cluster            Cluster           `json:"cluster"`
-	Name               string            `json:"name"`
-	Code               string            `json:"-"`
-	Version            string            `json:"version"`
-	EnforceSchema      bool              `json:"enforce_schema"`
-	HandlerUUID        int               `json:"handler_uuid"`
-	FunctionInstanceID string            `json:"function_instance_id"`
-	MetadataKeyspace   Keyspace          `json:"metadata_keyspace"`
-	SourceKeyspace     Keyspace          `json:"source_keyspace"`
-	BucketBindings     []BucketBinding   `json:"bucket_bindings"`
-	URLBindings        []URLBinding      `json:"url_bindings"`
-	ConstantBindings   []ConstantBinding `json:"constant_bindings"`
-	Settings           Settings          `json:"settings"`
+	Cluster          Cluster           `json:"cluster"`
+	Code             string            `json:"-"`
+	Name             string            `json:"name"`
+	MetadataKeyspace Keyspace          `json:"metadata_keyspace"`
+	SourceKeyspace   Keyspace          `json:"source_keyspace"`
+	BucketBindings   []BucketBinding   `json:"bucket_bindings"`
+	URLBindings      []URLBinding      `json:"url_bindings"`
+	ConstantBindings []ConstantBinding `json:"constant_bindings"`
+	Settings         Settings          `json:"settings"`
 }
 
-// Cluster represents couchbase connection information.
+// Cluster represents an couchbase cluster.
 type Cluster struct {
 	ConnectionString string `json:"connection_string"`
 	User             string `json:"user"`
@@ -35,21 +31,23 @@ type Keyspace struct {
 
 // BucketBinding represents an eventing function binding allowing the function access to buckets, scopes, and collections.
 type BucketBinding struct {
-	Alias  string   `json:"alias"`
-	Name   Keyspace `json:"name"`
-	Access string   `json:"access"`
+	Alias      string `json:"alias"`
+	Bucket     string `json:"bucket"`
+	Scope      string `json:"scope"`
+	Collection string `json:"collection"`
+	Access     string `json:"access"`
 }
 
 // URLBinding represents an eventing function binding allowing the function access external resources via cURL.
 type URLBinding struct {
 	Hostname               string `json:"hostname"`
 	Alias                  string `json:"alias"`
-	Auth                   Auth   `json:"auth"`
 	AllowCookies           bool   `json:"allow_cookies"`
 	ValidateSSLCertificate bool   `json:"validate_ssl_certificate"`
+	Auth                   Auth   `json:"auth"`
 }
 
-// Auth represents an URLBinding authentication method for an eventing function.
+// Auth represents an authentication method for URLBinding for an eventing function.
 type Auth struct {
 	Type  string `json:"type"`
 	User  string `json:"user"`
@@ -63,35 +61,14 @@ type ConstantBinding struct {
 	Literal string `json:"literal"`
 }
 
-// Settings are the settings for a Function.
+// Settings are the settings for an eventing Function.
 type Settings struct {
-	CPPWorkerThreadCount   int           `json:"cpp_worker_thread_count"`
-	DCPStreamBoundary      string        `json:"dcp_stream_boundary"`
-	Description            string        `json:"description"`
-	DeploymentStatus       bool          `json:"deployment_status"`
-	ProcessingStatus       bool          `json:"processing_status"`
-	LanguageCompatibility  string        `json:"language_compatibility"`
-	LogLevel               string        `json:"log_level"`
-	ExecutionTimeout       time.Duration `json:"execution_timeout"`
-	LCBInstCapacity        int           `json:"lcb_inst_capacity"`
-	LCBRetryCount          int           `json:"lcb_retry_count"`
-	LCBTimeout             time.Duration `json:"lcb_timeout"`
-	QueryConsistency       int           `json:"query_consistency"`
-	NumTimerPartitions     int           `json:"num_timer_partitions"`
-	SockBatchSize          int           `json:"sock_batch_size"`
-	TickDuration           time.Duration `json:"tick_duration"`
-	TimerContextSize       int           `json:"timer_context_size"`
-	UserPrefix             string        `json:"user_prefix"`
-	BucketCacheSize        int           `json:"bucket_cache_size"`
-	BucketCacheAge         int           `json:"bucket_cache_age"`
-	CurlMaxAllowedRespSize int           `json:"curl_max_allowed_resp_size"`
-	QueryPrepareAll        bool          `json:"query_prepare_all"`
-	WorkerCount            int           `json:"worker_count"`
-	HandlerHeaders         []string      `json:"handler_headers"`
-	HandlerFooters         []string      `json:"handler_footers"`
-	EnableAppLogRotation   bool          `json:"enable_app_log_rotation"`
-	AppLogDir              string        `json:"app_log_dir"`
-	AppLogMaxSize          int           `json:"app_log_max_size"`
-	AppLogMaxFiles         int           `json:"app_log_max_files"`
-	CheckpointInterval     time.Duration `json:"checkpoint_interval"`
+	DCPStreamBoundary     string        `json:"dcp_stream_boundary"`
+	Description           string        `json:"description"`
+	LogLevel              string        `json:"log_level"`
+	QueryConsistency      uint          `json:"query_consistency"`
+	WorkerCount           uint          `json:"worker_count"`
+	LanguageCompatibility string        `json:"language_compatibility"`
+	ExecutionTimeout      time.Duration `json:"execution_timeout"`
+	TimerContextSize      uint          `json:"timer_context_size"`
 }
